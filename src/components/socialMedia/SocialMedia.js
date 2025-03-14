828d8c2da7a2b95e4cd47919769860e0cae27c49
John B <john.bernal11@misena.edu.co>
2025-03-14 16:03:16 -0500

    Initial commit

---
+++ src/components/socialMedia/SocialMedia.js
@@ -0,0 +1,61 @@
+import React from "react";
+import "./SocialMedia.scss";
+import {socialMediaLinks} from "../../portfolio";
+
+export default function socialMedia() {
+  if (!socialMediaLinks.display) {
+    return null;
+  }
+  return (
+    <div className="social-media-div">
+
+      {socialMediaLinks.linkedin ? (
+        <a
+          href={socialMediaLinks.linkedin}
+          className="icon-button linkedin"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fab fa-linkedin-in"></i>
+          <span></span>
+        </a>
+      ) : null}
+
+      {socialMediaLinks.gmail ? (
+        <a
+          href={`mailto:${socialMediaLinks.gmail}`}
+          className="icon-button google"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fas fa-envelope"></i>
+          <span></span>
+        </a>
+      ) : null}
+
+      {socialMediaLinks.facebook ? (
+        <a
+          href={socialMediaLinks.facebook}
+          className="icon-button facebook"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fab fa-facebook-f"></i>
+          <span></span>
+        </a>
+      ) : null}
+
+      {socialMediaLinks.instagram ? (
+        <a
+          href={socialMediaLinks.instagram}
+          className="icon-button instagram"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fab fa-instagram"></i>
+          <span></span>
+        </a>
+      ) : null}
+    </div>
+  );
+}
